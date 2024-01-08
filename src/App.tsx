96097704c6a24a85714a5c3c5f87e6db1175117b
seymakeskin <mesya988@gmail.com>
2024-01-08 18:02:40 +0300

    productsları çektim, mapledim urunun bilgilerini yerleştirmedim henüz

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
+
 import './App.css';
 import  "./index.css"
 import Header from './components/Header';
+import Product from './components/Product';
+
+interface product {
+  id: number,
+  title: string,
+  description: string,
+  price: number,
+  discountPercentage:number,
+  rating:number,
+  stock:number,
+  brand:string,
+  category:string,
+  thumbnail:string,
+}
 
 function App() {
+ 
+  const baseUrl = 'https://dummyjson.com/products';
+  const [products , setProducts]= useState<any[]>();
+
+  useEffect(() => {
+    fetch(baseUrl).then((response) => {
+      if (!response.ok) {
+          throw (response.status);
+      }
+      return response.json();
+    }).then((responseData) => {
+      setProducts(responseData.products);
+    }).catch((error) => {
+       console.log('error',error);
+    });
+  },[])
+
+
   return (
-    <div className="App">
+    <div className="App bg-slate-100 h-screen">
         <Header></Header>
+        <div className="flex justify-center wrap">
+          {products && Array.isArray(products) ?(
+          <>
+              {products.map((product:Array<String>) => (
+                  <Product item={product}/>
+              ))} 
+            </>
+          ):(
+              <h1 className='text-slate-500 font-medium text-center'>Loading...</h1>
+          )}
+        </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html","./node_modules/flowbite/**/*.js" ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
 }
