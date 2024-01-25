5accb6780323c6e9ceb7c539cd30c0f666db9304
seymakeskin <mesya988@gmail.com>
2024-01-25 13:58:15 +0300

    istek atarak gelen user bilgileri local storage da tutulup eğer storage da token mecvutsa hesabım alanı gösterilecek şekilde ayarlama yapıldı.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,48 +2,55 @@ import React,{ useState , useEffect,} from 'react'
 
 export default function Login() {
     const [mail , setMail]= useState<string>();
+    const [username , setUsername]= useState<string>();
     const [password , setPassword]= useState<string>();
-    const [isLogedIn,setIsLogedIn] = useState<boolean>(false);
+    const [isLogedIn,setIsLogedIn] = useState<string>('');
 
     const submitEvent = (e:React.FormEvent) => {
         e.preventDefault();
-        if (mail !== "" && password !== "") {
-            setIsLogedIn(true);
+        if (username !== "" && password !== "") {
+            fetch('https://dummyjson.com/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                })
+            }).then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to login');
+                }
+            }).then(data => {
+                setIsLogedIn(data.token)
+                localStorage.setItem("userInfo", JSON.stringify(data));
+            })
+            .catch(error => {
+                alert(error);
+            });
         }else{
             alert("Lütfen bilgileri eksiksiz giriniz!");
         }
     };
 
     useEffect(() => {
-        if (isLogedIn) {
-            let Data = {
-                Email: mail,
-                Password: password,
-                isLogedIn: isLogedIn,
-            };
-            localStorage.setItem("userInfo", JSON.stringify(Data));
-        }
-    }, [isLogedIn, mail, password]);
-
-    const handleLogout = () => {
-        localStorage.removeItem('userInfo');
-        setIsLogedIn(false);
-    };
-
-    useEffect(() => {
         const logedInUser = localStorage.getItem('userInfo');
         if (logedInUser) {
             const parsedData = JSON.parse(logedInUser);
-            setIsLogedIn(parsedData.isLogedIn)
+            setIsLogedIn(parsedData.token)
         }
     }, [isLogedIn]);
 
-  
-
+    const handleLogout = () => {
+        localStorage.removeItem('userInfo');
+        setIsLogedIn('');
+    };
   return (
     <>
         {isLogedIn ? (
             <div className="container mx-auto mt-8">
+                <h1>Giriş Başarılı!</h1>
                 <h1 className="text-2xl font-bold text-gray-700 px-6 md:px-0">Hesabım</h1>
                 <button onClick={handleLogout} className="flex  justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Çıkış Yap</button>
             </div>
@@ -59,9 +66,9 @@ export default function Login() {
                          <div className="mt-2">
                              <input id="email" name="email"
                                 onChange={(e:any)=>{
-                                    setMail( e.target.value)    
+                                    setUsername( e.target.value)    
                                 }} 
-                             type="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                             type="text" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                          </div> 
                      </div>
                      <div>
