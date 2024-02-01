381f66784a81edf81704a80a7f2608a977be5d6a
seymakeskin <mesya988@gmail.com>
2024-02-01 11:50:02 +0300

    addto card button' a ekleme fonksiyonu eklendi

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Rating from './Rating'
 import { Link } from 'react-router-dom';
 import { ProductInterface } from '../types';
+import { CartContext } from '../contexts/CartContext';
 
 
 export default function Product({product}:{product:ProductInterface}) {
+    const { setOpen } = useContext(CartContext);
+    
+    function handleClick(e: React.FormEvent, d: any) {
+        // d:object yazınca hata ? 
+        
+        fetch('https://6585857f022766bcb8c8cfb7.mockapi.io/cart')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch cart. Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((cartItems) => {
+                const existingItem = cartItems.find((item: any) => item.productId === d.productId);
+                console.log('existingItem',existingItem)
+                if (existingItem) {
+                    const updatedItem = {
+                        ...existingItem,
+                        quantity: existingItem.quantity + 1,
+                    };
+
+                    return fetch(`https://6585857f022766bcb8c8cfb7.mockapi.io/cart/${existingItem.id}`, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(updatedItem),
+                    });
+                    
+                } else {
+                    return fetch('https://6585857f022766bcb8c8cfb7.mockapi.io/cart', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(d),
+                    });
+                }
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to update cart. Status: ${response.status}`);
+                }
+            
+                return response.json();
+            })
+            .then((da) => {
+                console.log('da', da);
+                setOpen(true);
+            })
+            .catch((error) => {
+                alert(error.message);
+        });
+    }
 
   return (
      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -21,7 +72,22 @@ export default function Product({product}:{product:ProductInterface}) {
                 </div>
             </Link>
             <div className="m-5">
-                <p className="text-white bg-slate-400 hover:bg-slate-500  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</p>
+                <p className="text-white bg-slate-400 hover:bg-slate-500  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={(e) => {
+                    e.preventDefault();
+                    var d = {
+                        productId: product.id,
+                        price: product.price,
+                        quantity: 1,
+                        thumbnail: product.thumbnail,
+                        title: product.title,
+                        total: product.price,
+                    }; 
+                    handleClick(e, d);
+                
+                }}  
+                
+                >Add to cart</p>
             </div>
      </div>
   )
